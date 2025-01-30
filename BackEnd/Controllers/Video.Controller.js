e38b0dc2003435b8ecee18e4c2163466ef28fcb9
kunaal-v <kunalfzk2001@gmail.com>
2025-01-30 19:28:06 +0530

    video update api done

--- BackEnd/Controllers/Video.Controller.js
+++ BackEnd/Controllers/Video.Controller.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import videoModel from "../Models/Video.Model.js";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
+
 dotenv.config();
 
 cloudinary.config({
@@ -25,8 +26,8 @@ try {
         tags:tags.split(","),
         videoUrl:uploadedVideo.secure_url,
         videoId:uploadedVideo.public_id,
-        thubmailId:uploadedThumbnail.secure_url,
-        thubmailUrl:uploadedThumbnail.public_id,
+        thubmailUrl:uploadedThumbnail.secure_url,
+        thubmailId:uploadedThumbnail.public_id,
     })
     const savedVideo=await newVideo.save();
     res.status(200).json([{message:"video uploaded"},{video:savedVideo}])
@@ -37,3 +38,44 @@ try {
 }
 }
 
+export async function updateVideo(req,res){
+    try {
+        const user=req.user;
+        const {title, category, description,tags}=req.body;
+        const videoId=req.params.id;
+        const video=await videoModel.findById(videoId);
+        if(user._id==video.user_id)
+        {
+            if(req.files)
+            {
+                await cloudinary.uploader.destroy(video.thubmailId);
+                const updatedThubmail=await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath);
+                const updatedVideo = await videoModel.findByIdAndUpdate(videoId,{
+                    title:title,
+                    description:description,
+                    category:category,
+                    tags:tags.split(","),
+                    thubmailId:updatedThubmail.public_id,
+                    thubmailUrl:updatedThubmail.secure_url,
+                },{new:true})
+                const savedVideo= await updatedVideo.save();
+                res.status(200).json([{message:"video updated"},{video:savedVideo}])
+
+            }
+            const updatedVideo = await videoModel.findByIdAndUpdate(videoId,{
+            title:title,
+            description:description,
+            category:category,
+            tags:tags.split(",")
+            },{new:true})
+            const savedVideo= await updatedVideo.save();
+            res.status(200).json([{message:"video updated"},{video:savedVideo}])
+        }
+        else{
+            return res.status(404).json({message:"you don't have permisson to update video"})
+        }
+    } catch (error) {
+       console.log(error);
+       return res.status(500).json({message:error})
+    }
+}
--- BackEnd/Routes/Video.Routes.js
+++ BackEnd/Routes/Video.Routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addVideo } from "../Controllers/Video.Controller.js";
+import { addVideo, updateVideo } from "../Controllers/Video.Controller.js";
 import { verifyToken } from "../MiddleWares/VerifyToken.js";
 
 const videoRoutes=express.Router();
 
-videoRoutes.post("/video",verifyToken,addVideo)
+videoRoutes.post("/video",verifyToken,addVideo);
+videoRoutes.put("/:id",verifyToken,updateVideo)
 export default videoRoutes;
