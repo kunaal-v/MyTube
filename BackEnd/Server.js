4031275d8ce9f8ede61c057deb3b699f0795aa66
kunaal-v <kunalfzk2001@gmail.com>
2025-02-04 12:15:31 +0530

    update in server.js

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import app from "../BackEnd/app.js"
 const server= http.createServer(app);
-const port=3000;
+const port=process.env.PORT ;
 server.listen(port,()=>{
     console.log("app is running on port: "+port);
 
