61433659d4a5321bada3a727300037fdef56e28c
kunaal-v <kunalfzk2001@gmail.com>
2025-02-04 12:27:40 +0530

    update in server.js

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,7 +1,9 @@
 import http from "http";
+import dotenv from 'dotenv';
 import app from "../BackEnd/app.js"
 const server= http.createServer(app);
 const port=process.env.PORT ;
+dotenv.config();
 server.listen(port,()=>{
     console.log("app is running on port: "+port);
 
