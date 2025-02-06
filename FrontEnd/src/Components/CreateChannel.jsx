b8f380dd546d68fdca204df346ef62754d13c1ce
kunaal-v <kunalfzk2001@gmail.com>
2025-02-06 11:28:43 +0530

    frontEnd vite project created

--- FrontEnd/src/Components/CreateChannel.jsx
+++
@@ -1,9 +0,0 @@
-
-function CreateChannel (){
-  return (
-    <>
-      <h1>CreateChannel Page</h1></>
-  )
-}
-
-export default CreateChannel
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,29 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import SignUp from './Components/SignUp.jsx'
-import CreateChannel from './Components/CreateChannel.jsx'
-const appRouter=createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:<App/>
-    },
-    {
-      path:"/home",
-      element:<App/>
-    },
-    {
-      path:"/signUp",
-      element:<SignUp/>
-    }
-    ,{
-      path:"/CreateChannel",
-      element:<CreateChannel/>
-    }
-  ]
-)
+
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={appRouter}/>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
