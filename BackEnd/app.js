b6b7ace812d289e813233fc0221b69db118c02d0
kunaal-v <kunalfzk2001@gmail.com>
2025-02-06 23:31:34 +0530

    update in videoController

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,34 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from 'dotenv';
-// import bodyParser from "body-parser";
-// import fileUpload from "express-fileupload";
-// import userRouter from "../BackEnd/Routes/User.Routes.js";
-// import cors from "cors"
-// import videoRoutes from "./Routes/Video.Routes.js";
-// import commentRoutes from "./Routes/Comment.Routes.js";
-// dotenv.config();
-
-
-// const app=express();
-// mongoose.connect(process.env.MONGO_URI)
-// .then((res)=>{
-//     console.log("conected with database");
-// })
-// .catch((err)=>{
-//     console.log(err);
-// })
-// app.use(cors())
-// app.use(bodyParser.json());
-// app.use(fileUpload({
-//     useTempFiles:true,
-//     tempFileDir:"/temp/"
-// }))
-
-// app.use(userRouter);
-// app.use(videoRoutes);
-// app.use(commentRoutes)
-// export default app;
 
 import express from "express";
 import mongoose from "mongoose";
